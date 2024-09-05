e444c7d851ac2b58c826e8b89a18677a2c7a2842
Noah Cauchi <njcauchi03@gmail.com>
2024-09-05 13:40:29 -0400

    read taken img

--- compression.py
+++ compression.py
@@ -4,15 +4,15 @@ from pylibdmtx.pylibdmtx import decode
 
 
 #change if multiple cameras
-cam_port = 0
+cam_port = 1
 
 cam = cv2.VideoCapture(cam_port) 
 success, image = cam.read()
 if success:
-    cv2.imwrite("test.png", image)
+    cv2.imwrite("img.png", image)
 
 
-im = cv2.imread('control2.png')
+im = cv2.imread('img.png')
 data = decode(im)
 name, loc = data[0]
 print(name)
