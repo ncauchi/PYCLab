bc999c2178c038e844251321425e99a4b05ca46a
Noah Cauchi <njcauchi03@gmail.com>
2024-09-16 14:05:56 -0400

    add

---
+++ compression.py
@@ -0,0 +1,54 @@
+
+import serial
+import time
+import cv2
+import os
+from pylibdmtx.pylibdmtx import decode
+
+
+#Serial Comm Setup
+pcPort = 'COM4'     #might be different for different computers
+arduino = serial.Serial(port=pcPort, baudrate=9600, timeout=.1) 
+
+#Camera Setup
+cam_port = 0
+cam = cv2.VideoCapture(cam_port) 
+sampleCode = ""
+
+
+
+while True:
+    #Get Name of Sample
+    success, image = cam.read()
+
+    if not success:
+        exit("Image capture error")
+
+    cv2.imwrite("img.png", image)
+    im = cv2.imread("img.png")
+    data = decode(im)
+
+    if(len(data) < 1):
+        exit("Could not decode data matrix from image")
+    
+    name, loc = data[0]
+    sampleCode = bytes.decode(name)
+    os.remove("img.png")
+    print("Starting test on sample: ", sampleCode)
+
+    #Tell UR5 to grab next sample
+    arduino.write(int(1).to_bytes() )
+    time.sleep(0.05)
+    res = arduino.readline().decode()
+
+    if(res != "On"):
+        exit("Arduino communication error")
+
+    print("Starting Test")
+
+    #Tell Instron to start
+    #Get Signal From Instron
+    inp = input("[dev] Test Complete (Y/N) ") 
+
+    if(inp != "Y" or inp != "y"):
+        exit("Exiting")
