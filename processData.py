fe23ea79950090d71f88d45761e1ec12a31167dd
Noah Cauchi <njcauchi03@gmail.com>
2024-10-28 20:28:16 -0400

    asd

--- processData.py
+++ processData.py
@@ -24,7 +24,12 @@ def process(dataFile, nameFile, startLine):
 
     for line in file:
         prevTime = currTime
-        currTime = float(line[1])
+        currTime_raw = line[1]
+        if(len(currTime_raw) == 0){
+            print("Data Processing Complete")
+            return
+        }
+        currTime = float(currTime_raw)
 
         if currTime - prevTime > 1 :
             if samp != None:
@@ -39,5 +44,5 @@ def process(dataFile, nameFile, startLine):
         
         output.writerow([currTime, line[2], line[3]])
 
-    print("Data Processing Complete")
+    
 
